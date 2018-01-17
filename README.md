7090db1bb452db9f4ccdb8e23637d763697c8a73
TangerineSpecter <993033472@qq.com>
2018-01-18 00:44:42 +0800

    v0.5.1 添加摩斯密码解密

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ### Java基本工具包：
 - 工具包地址：https://github.com/TangerineSpecter/JavaBaseUtils
 
-[![LatestVersion](https://img.shields.io/badge/LatestVersion-0.5.0-orange.svg)](https://github.com/TangerineSpecter/JavaBaseUtils/blob/master/VERSION.md)
+[![LatestVersion](https://img.shields.io/badge/LatestVersion-0.5.1-orange.svg)](https://github.com/TangerineSpecter/JavaBaseUtils/blob/master/VERSION.md)
 
 最新版本更新记录
 
-- V0.5.0（2018-01-17）
-	- 添加解密工具类
+- V0.5.1（2018-01-18）
+	- 添加摩斯密码解密
--- src/common/util/DecipheringUtils.java
+++ src/common/util/DecipheringUtils.java
@@ -1,5 +1,8 @@
 package common.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 解密工具类
  * 
@@ -8,4 +11,49 @@ package common.util;
  */
 public class DecipheringUtils {
 
+	/** 摩斯密码 */
+	private static Map<String, Object> morseCodeMap = new HashMap<>();
+	/** 摩斯key */
+	private static final String[] morse_key = ".----,..---,...--,....-,.....,-....,--...,---..,----.,-----,.-,-...,-.-.,-..,.,..-.,--.,....,..,.---,-.-,.-..,--,-.,---,.--.,--.-,.-.,...,-,..-,...-,.--,-..-,-.--,--.."
+			.split(",");
+	/** 摩斯value */
+	private static final String[] morse_value = "1,2,3,4,5,6,7,8,9,0,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
+			.split(",");
+
+	static {
+		morseCodeMap = getMorseCode();
+	}
+
+	/**
+	 * 获取摩斯密码
+	 * 
+	 * @return
+	 */
+	public static Map<String, Object> getMorseCode() {
+		Map<String, Object> morseMap = new HashMap<>();
+		if (morse_key.length == morse_value.length) {
+			for (int index = 0; index < morse_key.length; index++) {
+				morseMap.put(morse_key[index], morse_value[index]);
+			}
+		} else {
+			System.out.println("摩斯密码key,value数量不对等!");
+		}
+		return morseMap;
+	}
+
+	/**
+	 * 解密摩斯密码
+	 * 
+	 * @param morseCode
+	 *            摩斯密码
+	 * @return
+	 */
+	public static String getMorseResult(String morseCode) {
+		String result = Constant.NULL_KEY_STR;
+		String[] morseArray = morseCode.split("/");
+		for (String morse : morseArray) {
+			result += morseCodeMap.get(morse);
+		}
+		return result;
+	}
 }

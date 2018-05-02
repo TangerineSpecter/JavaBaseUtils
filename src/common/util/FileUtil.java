c986bf53765aeec01883ec92fc1c675817a4ee01
TangerineSpecter <993033472@qq.com>
2018-05-02 09:58:38 +0800

    获取路径下目录和文件

--- src/common/util/FileUtil.java
+++ src/common/util/FileUtil.java
@@ -2,6 +2,8 @@ package common.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
@@ -79,5 +81,64 @@ public class FileUtil {
 		byte[] data = Base64.decodeBase64(base64);
 		data = RarUtils.unGZip(data);
 		writeFile(data, fileName, path);
+	}
+
+	/**
+	 * 获取路径下的所有文件/文件夹
+	 * 
+	 * @param directoryPath
+	 *            需要遍历的文件夹路径
+	 * @param isAddDirectory
+	 *            是否将子文件夹的路径也添加到list集合中
+	 * @return
+	 */
+	public static List<String> getAllFilePath(String directoryPath, boolean isAddDirectory) {
+		List<String> list = new ArrayList<String>();
+		File baseFile = new File(directoryPath);
+		if (baseFile.isFile() || !baseFile.exists()) {
+			return list;
+		}
+		File[] files = baseFile.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				if (isAddDirectory) {
+					list.add(file.getAbsolutePath());
+				}
+				list.addAll(getAllFilePath(file.getAbsolutePath(), isAddDirectory));
+			} else {
+				list.add(file.getAbsolutePath());
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 获取路径下的所有文件名
+	 * 
+	 * @param directoryPath
+	 *            需要遍历的文件夹路径
+	 * @param isPostfix
+	 *            是否切割后缀
+	 * @return
+	 */
+	public static List<String> getAllFileName(String directoryPath, boolean isPostfix) {
+		List<String> list = new ArrayList<String>();
+		File baseFile = new File(directoryPath);
+		if (baseFile.isFile() || !baseFile.exists()) {
+			return list;
+		}
+		File[] files = baseFile.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				list.addAll(getAllFileName(file.getName(), isPostfix));
+			} else {
+				if (isPostfix) {
+					list.add(file.getName().substring(0, file.getName().lastIndexOf(".")));
+				} else {
+					list.add(file.getName());
+				}
+			}
+		}
+		return list;
 	}
 }
